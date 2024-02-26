e839e112e23fa4d1657d3819872072f50d90b429
Виктория <vickhodulova@gmail.com>
2024-02-26 15:11:43 +0900

    feat: add About component content

---
+++ src/components/About/About.tsx
@@ -0,0 +1,10 @@
+export const About = () => {
+  return (
+    <section className="about">
+      <h2> О Нас </h2>
+      <div className="about__text_wrapper">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, rem molestiae ab sed non vitae vero animi dolore officiis quam, a est quo provident accusantium ad distinctio hic optio voluptates!</p>
+      </div>
+    </section>
+  )
+}
