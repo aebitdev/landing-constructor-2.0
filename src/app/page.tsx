fabf0a2bf8301bac5a6812e3963ac0d3f9de4d8e
Виктория <vickhodulova@gmail.com>
2024-03-01 15:27:08 +0900

    feat: add changes to eslintrc, tsconfig, page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,86 +1,93 @@
-import React from "react";
-import Image from "next/image";
-import styles from "./page.module.css";
-import About from "@/components/About/About";
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header/Header";
-import Top from "@/components/Top/Top";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      
-      <div className={styles.description}>
-        <Header />
-      </div>
-      <Top />
+import React from 'react'
+import Image from 'next/image'
+import styles from './page.module.css'
+import About from '@/components/About/About'
+import Footer from '@/components/Footer/Footer'
+import Header from '@/components/Header/Header'
+import Top from '@/components/Top/Top'
 
-      <div className={styles.center}>
-        
-      <Image
-          className={styles.logo}
-          src="/img/logo.svg"
-          alt="Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-      <About/>
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+const Home: React.FC = () => {
+    return (
+        <main className={styles.main}>
+            <div className={styles.description}>
+                <Header />
+            </div>
+            <Top />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            <div className={styles.center}>
+                <Image
+                    className={styles.logo}
+                    src="/img/logo.svg"
+                    alt="Logo"
+                    width={180}
+                    height={37}
+                    priority
+                />
+            </div>
+            <About />
+            <div className={styles.grid}>
+                <a
+                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Docs <span>-&gt;</span>
+                    </h2>
+                    <p>
+                        Find in-depth information about Next.js features and
+                        API.
+                    </p>
+                </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+                <a
+                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Learn <span>-&gt;</span>
+                    </h2>
+                    <p>
+                        Learn about Next.js in an interactive course
+                        with&nbsp;quizzes!
+                    </p>
+                </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-      
-      <Footer/>
-    </main>
-    
-  );
+                <a
+                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Templates <span>-&gt;</span>
+                    </h2>
+                    <p>Explore starter templates for Next.js.</p>
+                </a>
+
+                <a
+                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        Deploy <span>-&gt;</span>
+                    </h2>
+                    <p>
+                        Instantly deploy your Next.js site to a shareable URL
+                        with Vercel.
+                    </p>
+                </a>
+            </div>
+
+            <Footer />
+        </main>
+    )
 }
+
+export default Home;
