e8f4e56951ec9dcd994c9760d9c0f95dcaa82770
Виктория <vickhodulova@gmail.com>
2024-03-01 15:44:18 +0900

    feat: add styles to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import Image from 'next/image';
-// import style from 'Header.module.scss';
+import React from 'react'
+import Image from 'next/image'
+import styles from './Header.module.scss'
 
 export default function Header() {
-  return (
-    <header>
-      <div className="logo">
-        <Image
-          src="/img/logo.svg"
-          alt="Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-      <div className="header__menu">
-        <nav>
-          <ul>
-            <li>
-              <a href="#about">О нас</a>
-            </li>
-            <li>
-              <a href="#faq">FAQ</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <Image
+                    src="/img/logo.svg"
+                    alt="Logo"
+                    width={180}
+                    height={37}
+                    priority
+                />
+            </div>
+            <div className={styles.header__menu}>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#about">О нас</a>
+                        </li>
+                        <li>
+                            <a href="#faq">FAQ</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
 }
